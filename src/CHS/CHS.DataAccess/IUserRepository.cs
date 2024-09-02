0987270123d0b1f8441f5a4d1b870bc81e27b399
Noah Nielsen <noahanielsen@gmail.com>
2024-09-02 23:16:26 +0200

    Add UserController and IUserRepository interfaces

---
+++ src/CHS/CHS.DataAccess/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using CHS.Entities;
+
+namespace CHS.DataAccess
+{
+    public interface IUserRepository
+    {
+        public User GetUserById(int id);
+
+        public User GetUserByEmail(string email);
+
+        public User GetUserByEmailAndPassword(string email, string password);
+
+        public void AddUser(User user);
+
+        public void UpdateUser(User user);
+
+        public void DeleteUser(User user);
+    }
+}
